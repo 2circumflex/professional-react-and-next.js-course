54825115430e961a799ed08416eff7d3232ad19d
roy <2circumflex@gmail.com>
2024-01-18 22:58:44 +0900

    CorpComment - Prevent Event Bubbling In The DOM

--- corp-comment/src/components/feedback/FeedbackItem.tsx
+++ corp-comment/src/components/feedback/FeedbackItem.tsx
@@ -1,6 +1,6 @@
+import React, { useState } from "react"
 import { TriangleUpIcon } from "@radix-ui/react-icons"
 import { TFeedbackItem } from "../../lib/types"
-import { useState } from "react"
 
 type FeedbackItemProps = {
   feedbackItem: TFeedbackItem
@@ -10,12 +10,17 @@ export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
   const [open, setOpen] = useState(false)
   const [upvoteCount, setUpvoteCount] = useState(feedbackItem.upvoteCount)
 
+  const handleUpvote = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setUpvoteCount((prev) => ++prev)
+    e.stopPropagation()
+  }
+
   return (
     <li
       className={`feedback ${open ? "feedback--expand" : ""}`}
       onClick={() => setOpen((prev) => !prev)}
     >
-      <button onClick={() => setUpvoteCount((prev) => ++prev)}>
+      <button onClick={handleUpvote}>
         <TriangleUpIcon />
         <span>{upvoteCount}</span>
       </button>
