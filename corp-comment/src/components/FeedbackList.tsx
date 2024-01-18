f257d41dacbc81eb338f16e750e38ea1eab26829
roy <2circumflex@gmail.com>
2024-01-18 20:33:21 +0900

    CorpComment - Top 3 Error When Fetching Data

--- corp-comment/src/components/FeedbackList.tsx
+++ corp-comment/src/components/FeedbackList.tsx
@@ -1,21 +1,32 @@
 import { useEffect, useState } from "react"
 import FeedbackItem from "./FeedbackItem"
 import Spinner from "./Spinner"
+import ErrorMessage from "./ErrorMessage"
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState("")
 
   useEffect(() => {
     setIsLoading(true)
+
     fetch(
       "https://bytegrad.com/api/course-assets/projects/corpcomment/api/feedbacks"
     )
       .then((response) => {
+        if (!response.ok) {
+          throw new Error("")
+        }
         return response.json()
       })
       .then((data) => {
         setFeedbackItems(data.feedbacks)
+        setIsLoading(false)
+      })
+      .catch(() => {
+        // network error, not 2xx response, or JSON parsing error
+        setErrorMessage("Something went wrong.")
         setIsLoading(false)
       })
   }, [])
@@ -23,6 +34,9 @@ export default function FeedbackList() {
   return (
     <ol className="feedback-list">
       {isLoading ? <Spinner /> : null}
+
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
+
       {feedbackItems.map((feedbackItem) => (
         <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
