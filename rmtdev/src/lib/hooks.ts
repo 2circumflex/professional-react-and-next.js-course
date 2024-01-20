238d47018ce5b42d3fad65a2f78214162c826d85
roy <2circumflex@gmail.com>
2024-01-20 20:57:32 +0900

    RmtDev - Debounce Searchtext (UseDebounce Custom Hook)

--- rmtdev/src/lib/hooks.ts
+++ rmtdev/src/lib/hooks.ts
@@ -65,3 +65,15 @@ export function useJobItems(searchText: string) {
 
   return { jobItemsSliced, isLoading, totalNumberOfResults } as const;
 }
+
+export function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState("");
+
+  useEffect(() => {
+    const timerId = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => clearTimeout(timerId);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
