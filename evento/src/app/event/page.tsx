9313beed472896218979dc3a38d7b5b52cfe5df2
Roy <2circumflex@gmail.com>
2024-02-02 19:49:09 +0900

    EVENTO - Event Page Params (Get Event Slug from URL)

--- evento/src/app/event/page.tsx
+++ evento/src/app/event/page.tsx
@@ -1,3 +1,9 @@
-export default function EventPage() {
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function EventPage({ params }: EventPageProps) {
   return <main>Event page</main>;
 }
