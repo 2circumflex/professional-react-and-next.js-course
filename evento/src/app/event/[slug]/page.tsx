58a906ffd0307b375fed8e05f99c58e2ca71142e
Roy <2circumflex@gmail.com>
2024-02-02 22:26:55 +0900

    EVENTO - Fetch Event From URL

---
+++ evento/src/app/event/[slug]/page.tsx
@@ -0,0 +1,15 @@
+type EventPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function EventPage({ params }: EventPageProps) {
+  const slug = params.slug;
+  const response = await fetch(
+    `https://bytegrad.com/course-assets/projects/evento/api/events/${slug}`
+  );
+  const event = await response.json();
+
+  return <main>Event page</main>;
+}
--- evento/src/app/event/page.tsx
+++
@@ -1,9 +0,0 @@
-type EventPageProps = {
-  params: {
-    slug: string;
-  };
-};
-
-export default async function EventPage({ params }: EventPageProps) {
-  return <main>Event page</main>;
-}
